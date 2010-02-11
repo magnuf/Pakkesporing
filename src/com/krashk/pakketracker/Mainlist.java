6eef9e2da800ae1018b4975d548dc3720f513539
magnuf <user@domain>
2010-02-11 23:34:02 +0000

    Created new Android project

---
+++ src/com/krashk/pakketracker/Mainlist.java
@@ -0,0 +1,13 @@
+package com.krashk.pakketracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Mainlist extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
