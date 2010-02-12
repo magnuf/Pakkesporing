be8ed8e31ca6f41595cf450f7e0eb8192656df2e
magnuf <user@domain>
2010-02-12 12:36:02 +0000

    Added a menu and some items

--- src/com/krashk/pakketracker/Mainlist.java
+++ src/com/krashk/pakketracker/Mainlist.java
@@ -1,15 +1,24 @@
 package com.krashk.pakketracker;
 
+import java.util.HashMap;
+
+import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
 public class Mainlist extends ListActivity {
 	
-	public static final int INSERT_ID = Menu.FIRST;
-     private PackagesDbAdapter packageDbAdapter;
+	public static final int REFRESH_ID = Menu.FIRST;
+	public static final int SETTINGS_ID = Menu.FIRST +1;
+    private PackagesDbAdapter packageDbAdapter;
 	
     /** Called when the activity is first created. */
     @Override
@@ -18,18 +27,32 @@ public class Mainlist extends ListActivity {
         setContentView(R.layout.listpackages);
         packageDbAdapter = new PackagesDbAdapter(this);
         packageDbAdapter.open();
-        fillData();
+        
+//        fillData();
+        
+        Button createNew = (Button) findViewById(R.id.newpackagebutton);
+        
+        OnClickListener add = new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		};
 
     }
 
-    /*
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
     	boolean result = super.onCreateOptionsMenu(menu);
-        menu.add(0, INSERT_ID, 0, R.string.menu_insert);
+        menu.add(0, REFRESH_ID, 0, R.string.refresh).setIcon(android.R.drawable.ic_menu_search);
+        menu.add(0, SETTINGS_ID, 0, R.string.settings).setIcon(android.R.drawable.ic_menu_preferences);
         return result;
     }
-
+    
+    /*
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
     	switch (item.getItemId()) {
@@ -55,9 +78,21 @@ public class Mainlist extends ListActivity {
         int[] to = new int[] {R.id.status, R.id.tracknumber};
         
         // Now create an array adapter and set it to display using our rowlayout
-        SimpleCursorAdapter notes =
+        SimpleCursorAdapter packages =
             new SimpleCursorAdapter(this, R.layout.listitem, c, from, to);
-        setListAdapter(notes);
+        setListAdapter(packages);
     }
+    
+    @Override
+	protected void onListItemClick (ListView l, View v, int position, long id){
+//		HashMap<String,String> item = (HashMap<String, String>) getListView().getItemAtPosition(position);
+//		
+//		String result = item.get("filename");
+//		
+//		Intent resultIntent = new Intent();
+//		resultIntent.putExtra(PUBLIC_STATIC_STRING_IDENTIFIER, result );
+//		setResult(Activity.RESULT_OK, resultIntent);
+//		finish();
+	}
 }
 
