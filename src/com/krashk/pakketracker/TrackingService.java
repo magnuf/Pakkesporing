793c043ce4c4a158717572b9fdbef9a92cbd9523
magnuf <user@domain>
2010-02-14 11:21:55 +0000

    Service

--- src/com/krashk/pakketracker/TrackingService.java
+++ src/com/krashk/pakketracker/TrackingService.java
@@ -4,28 +4,33 @@ import java.io.IOException;
 
 import org.apache.http.client.ClientProtocolException;
 
-import android.app.Activity;
+import android.app.Service;
+import android.content.Intent;
 import android.database.Cursor;
-import android.os.Bundle;
+import android.os.IBinder;
 
-public class TrackingService extends Activity {
+public class TrackingService extends Service {
 
 	private PackagesDbAdapter packagesDbAdapter;
 	
-    @Override
-    public void onCreate(Bundle bundle) {
-    	packagesDbAdapter = new PackagesDbAdapter(this);
-    	packagesDbAdapter.open();
-    	
+	
+	@Override
+	public void onCreate(){
+		packagesDbAdapter = new PackagesDbAdapter(this);
+		packagesDbAdapter.open();
+	}
+	
+	@Override
+    public void onStart(Intent intent, int startId) {
     	Cursor c =  packagesDbAdapter.fetchAllPackages();
     	
     	if (c.moveToFirst()){
     		do {
     			int packageid = c.getInt(c.getColumnIndex(PackagesDbAdapter.KEY_ID));
-    	    	String packageNumber = c.getString(c.getColumnIndex(PackagesDbAdapter.KEY_NUMBER));
-    	    	String oldStatus = c.getString(c.getColumnIndex(PackagesDbAdapter.KEY_STATUS));
-    	    	try {
-    	    		String newStatus = TrackingUtils.updateStatus(packageNumber, oldStatus);
+    			String packageNumber = c.getString(c.getColumnIndex(PackagesDbAdapter.KEY_NUMBER));
+    			String oldStatus = c.getString(c.getColumnIndex(PackagesDbAdapter.KEY_STATUS));
+    			try {
+    				String newStatus = TrackingUtils.updateStatus(packageNumber, oldStatus);
     				if (newStatus != null){
     					packagesDbAdapter.updatePackage(packageid, newStatus, R.attr.changed);
     				}
@@ -36,8 +41,18 @@ public class TrackingService extends Activity {
     			}
     		} while (c.moveToNext());
     	}
-    	packagesDbAdapter.close();
+    	
     }
+	@Override
+	public void onDestroy() {
+		packagesDbAdapter.close();
+	}
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		return null;
+	}
+
 
 
 }
